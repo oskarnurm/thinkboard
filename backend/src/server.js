b5cc7b49c558384b2f5ffb595c91479846b03177
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-06-03 21:43:11 +0200

    chore: setup dotenv and MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
 import express from "express";
-import notesRoutes from "../routes/notesRoutes.jss.js";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
-  console.log("server started on PORT: 5001");
+connectDB();
+
+app.listen(PORT, () => {
+  console.log("server started on PORT: " + PORT);
 });
+
+// mongodb+srv://oskarnurm:thinkboard@cluster0.mat1k1a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
