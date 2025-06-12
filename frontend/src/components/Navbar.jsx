be00e340ee40abf46bf93fa7319b203bb5f1dc5d
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-06-12 22:44:25 +0200

    feat: add NoteCard and RateLimitUI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { PlusIcon } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <header className="bg-base-300 border-b border-base-conten/10">
+    <header className="bg-base-300 border-b border-base-content/10">
       <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
-            ThinkBoard{" "}
+            ThinkBoard
           </h1>
           <div className="flex items-center gap-4">
             <Link to={"/create"} className="btn btn-primary">
@@ -20,5 +20,4 @@ const Navbar = () => {
     </header>
   );
 };
-
 export default Navbar;
