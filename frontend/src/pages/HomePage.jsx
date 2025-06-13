81d1b147d1d04a3f1f4d31f39b0d3fe7543c3523
oskarnurm <19738295+oskarnurm@users.noreply.github.com>
2025-06-13 12:18:50 +0200

    feat: add NotesNotFound page when there are not notes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from "../components/RateLimitedUI";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
 import api from "../lib/axios";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [IsRateLimited, setRateLimited] = useState(false);
@@ -41,6 +42,10 @@ const HomePage = () => {
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {isLoading && (
           <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+
+        {!isLoading && notes.length === 0 && !IsRateLimited && (
+          <NotesNotFound />
         )}
 
         {notes.length > 0 && !IsRateLimited && (
